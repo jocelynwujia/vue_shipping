00e58c7bdf4647dcd62b685dd615eed8deec5a33
jocelyn <499264478@qq.com>
2020-11-28 11:24:29 +0800

    06.登录表单数据验证

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,40 +1,63 @@
 <template>
   <div class="login_container">
-      <div class="login_box">
-          <!-- 头像区域 -->
-          <div class="avater_box">
-              <img src="../assets/logo.png" alt="">
-          </div>
-          <!-- 表单区域 -->
-          <el-form :model = "loginForm" label-width="0px" class="login_form">
-            <!-- 用户名 -->
-            <el-form-item>
-                <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
-            </el-form-item>
-            <!-- 密码 -->
-            <el-form-item>
-                <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
-            </el-form-item>
-            <!-- 按钮 -->
-            <el-form-item class="btns">
-                <el-button type="primary">登录</el-button>
-                <el-button type="info">重置</el-button>
-            </el-form-item>
-          </el-form>
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avater_box">
+        <img src="../assets/logo.png" alt="" />
       </div>
+      <!-- 表单区域 -->
+      <el-form
+        :model="loginForm"
+        :rules="loginformRules"
+        label-width="0px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop = "username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"         
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop = "password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    data (){
-        return {
-            loginForm :{
-                username:'zs',
-                password:'123456'
-            }
-        }
-    }
+  data() {
+    return {
+      loginForm: {
+        username: "zs",
+        password: "123456",
+      },
+      //登录表单的校验
+      loginformRules: {
+        username: [
+          { required: true, message: "请输入登录名称", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" },
+        ],
+        password:[
+          { required: true, message: "请输入用户密码", trigger: "blur" },
+          { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" },  
+        ]
+      },
+    };
+  },
 };
 </script>
 
@@ -68,18 +91,18 @@ export default {
       height: 100%;
       border-radius: 50%;
       background-color: #eee;
-    }   
+    }
   }
   .login_form {
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-      padding: 0 20px;
-      box-sizing: border-box;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
   }
   .btns {
-        display: flex;
-        justify-content: flex-end;
-    }
+    display: flex;
+    justify-content: flex-end;
+  }
 }
 </style>>
